25c2b4432b6a648d91dccb4782765b6c180e8b0e
anjoe-people10 <anjoe.nambadan@people10.com>
2024-04-16 14:08:44 +0530

    final servlets

--- src/com/people10/servlet/demo/AddServlet.java
+++ src/com/people10/servlet/demo/AddServlet.java
@@ -1,18 +1,30 @@
 package com.people10.servlet.demo;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/add")
-public class AddServlet extends HttpServlet{
+public class AddServlet extends HttpServlet {
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int result = i+j;
+		
+		result = result*result;
+		
+		req.setAttribute("k", result);
+		
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("square");
+		requestDispatcher.forward(req, res);
 	}
 }
